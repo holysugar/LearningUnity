eda16423102e97c2fbe6f9bfda4346ffbd2e2b93
holy <holysugar@gmail.com>
2013-10-23 18:08:18 +0900

    rotation for iOS

--- UnityBalanceGame/Assets/KeyMove.cs
+++ UnityBalanceGame/Assets/KeyMove.cs
@@ -10,7 +10,14 @@ public class KeyMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// reverse rotation from the textbook
+		// for Keyboard
+		/*
 		transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 30.0f * Time.deltaTime, new Vector3( 0.0f, 0.0f, -1.0f));
 		transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Vertical")   * 30.0f * Time.deltaTime, new Vector3(-1.0f, 0.0f,  0.0f));
+		*/
+		// for iOS
+		transform.rotation = 
+			Quaternion.AngleAxis(Input.acceleration.x * 60, new Vector3( 0.0f, 0.0f, -1.0f)) *
+			Quaternion.AngleAxis(Input.acceleration.y * 60, new Vector3( 1.0f, 0.0f,  0.0f));
 	}
 }
