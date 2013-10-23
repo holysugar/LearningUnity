f46a65025153aaf5f76e770e792b03cb8a5e8dd6
holysugar <holysugar@gmail.com>
2013-10-23 10:56:29 +0900

    reverse rotation

--- Assets/KeyMove.cs
+++ Assets/KeyMove.cs
@@ -9,7 +9,8 @@ public class KeyMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 30.0f * Time.deltaTime, new Vector3(0.0f, 0.0f, 1.0f));
-		transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Vertical")   * 30.0f * Time.deltaTime, new Vector3(1.0f, 0.0f, 0.0f));
+		// reverse rotation from the textbook
+		transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 30.0f * Time.deltaTime, new Vector3( 0.0f, 0.0f, -1.0f));
+		transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Vertical")   * 30.0f * Time.deltaTime, new Vector3(-1.0f, 0.0f,  0.0f));
 	}
 }
